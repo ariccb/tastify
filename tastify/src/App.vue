44d656e92d05ea67202e6b3d36b84b1157d14f2f
Aric Crosson Bouwers <aricbouwers@outlook.com>
2020-10-28 09:25:44 -0600

    Playlist Titles showing up in grid format

--- tastify/src/App.vue
+++ tastify/src/App.vue
@@ -1,125 +1,122 @@
 <template>
-<div id="app">
+  <div id="app">
     <div id="nav">
-        <v-card class="overflow-hidden">
-            <v-app-bar
-                absolute
-                color="#43a047"
-                dark
-                shrink-on-scroll
-                prominent
-                src="https://picsum.photos/1920/1080?random"
-                fade-img-on-scroll
-                scroll-target="#scrolling-techniques-5"
-                scroll-threshold="5">
-                <template v-slot:img="{ props }">
-                    <v-img
-                        v-bind="props"
-                        gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"></v-img>
-                </template>
+      <v-card class="overflow-hidden">
+        <v-app-bar
+          absolute
+          color="#43a047"
+          dark
+          shrink-on-scroll
+          prominent
+          src="https://picsum.photos/1920/1080?random"
+          fade-img-on-scroll
+          scroll-target="#scrolling-techniques-5"
+          scroll-threshold="5"
+        >
+          <template v-slot:img="{ props }">
+            <v-img
+              v-bind="props"
+              gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+            ></v-img>
+          </template>
 
-                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-                <v-toolbar-title>Tastify
-                    <v-card-subtitle>
-                        {{ mainUser[0].name }}
-                    </v-card-subtitle>
-                </v-toolbar-title>
+          <v-toolbar-title
+            >Tastify
+            <v-card-subtitle>
+              {{ mainUser[0].name }}
+            </v-card-subtitle>
+          </v-toolbar-title>
 
-                <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
 
-                <v-btn icon>
-                    <v-icon>mdi-heart
-                        
-                        <!-- insert spotify account picture -->
-                    </v-icon>
-                </v-btn>
+          <v-btn icon>
+            <v-icon
+              >mdi-heart
 
-                <template v-slot:extension>
-                    <v-tabs align-with-title>
-                        <v-tab>
-                            <router-link to="/">Login</router-link>
-                        </v-tab>
-                        <v-tab>
-                            <router-link to="/MainPage">Playlists</router-link>
-                        </v-tab>
-                    </v-tabs>
-                </template>
-            </v-app-bar>
-            <v-sheet
-                id="scrolling-techniques-5"
-                class="overflow-y-auto"
-                max-height="224">
-                <v-container style="height: 224px;"></v-container>
-            </v-sheet>
-        </v-card>
+              <!-- insert spotify account picture -->
+            </v-icon>
+          </v-btn>
 
+          <template v-slot:extension>
+            <v-tabs align-with-title>
+              <v-tab>
+                <router-link to="/">Login</router-link>
+              </v-tab>
+              <v-tab>
+                <router-link to="/MainPage">Playlists</router-link>
+              </v-tab>
+            </v-tabs>
+          </template>
+        </v-app-bar>
+        <v-sheet
+          id="scrolling-techniques-5"
+          class="overflow-y-auto"
+          max-height="224"
+        >
+          <v-container style="height: 224px"></v-container>
+        </v-sheet>
+      </v-card>
     </div>
     <router-view />
-</div>
+  </div>
 </template>
 
 <style lang="scss">
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
 #nav {
-    padding: 30px;
+  padding: 30px;
 
-    a {
-        font-weight: bold;
-        color: #2c3e50;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-        &.router-link-exact-active {
-            color: #42b983;
-        }
+    &.router-link-exact-active {
+      color: #42b983;
     }
+  }
 }
 </style>
 
 <script lang="ts">
-import LandingPage from './views/LandingPage.vue'
-import axios from 'axios'
+import LandingPage from "./views/LandingPage.vue";
+import axios from "axios";
 
-export default ({
-    name: 'App',
+export default {
+  name: "App",
+  components: {
+    LandingPage,
+  },
 
-    components: {
-        LandingPage,
-    },
+  data() {
+    return {
+      mainUser: [],
+    };
+  },
 
-    data: () => {
-        return {
-            mainUser: [
-                
-            ],
-                        
-        }
-    },
-
-     async mounted(){
-      let users = await axios.get('http://localhost:3000/user').then((res) => {              
-            //   console.log(res.data)
-              let user = {
-                  name: res.data.name,
-                  id: res.data._id,
-                  images: res.data.images,
-                  country: res.data.country,
-                  url: res.data.url,
-                  playlists: res.data.playlists[0].items
-              }
-              this.mainUser.push(user) 
-              console.log('um i think this is working on App.vue')
-              console.log(user)
-              
-      })
-     }
-})
-    
-
+  async mounted() {
+    let users = await axios.get("http://localhost:3000/user").then((res) => {
+      //   console.log(res.data)
+      let user = {
+        name: res.data.name,
+        id: res.data._id,
+        images: res.data.images,
+        country: res.data.country,
+        url: res.data.url,
+        playlists: res.data.playlists[0].items,
+      }
+      this.mainUser.push(user);
+      console.log("um i think this is working on App.vue");
+      console.log(user);
+    })
+  },
+}
 </script>
--- tastify/src/views/MainPage.vue
+++ tastify/src/views/MainPage.vue
@@ -1,129 +1,105 @@
 <template>
-<div>
+  <div>
     <v-container
-        v-for="align in alignments"
-        :key="align"
-        class="grey lighten-5 mb-6">
-        <v-row
-            :align="align"
-            no-gutters
-            style="height: 150px;">
-            <v-col
-                v-for="n in 3"
-                :key="n">
-                <v-card
-                    class="pa-2"
-                    outlined
-                    tile>
-                    One of three columns
-                </v-card>
-            </v-col>
-        </v-row>
+      v-for="align in alignments"
+      :key="align"
+      class="grey lighten-5 mb-6"
+    >
+      <v-row :align="align" no-gutters style="height: 150px">
+        <v-col v-for="n in 3" :key="n">
+          <v-card class="pa-2" outlined tile> <div > {{ mainUser[0].playlists[0].name }}</div> </v-card>
+        </v-col>
+      </v-row>
     </v-container>
 
     <div id="app">
-        <v-app id="inspire">
-            <div class="text-center">
-                <v-bottom-sheet inset>
-                    <template v-slot:activator="{ on, attrs }">
-                        <v-btn
-                            color="red"
-                            dark
-                            v-bind="attrs"
-                            v-on="on">
-                            Open Player
-                        </v-btn>
-                    </template>
-                    <v-card tile>
-                        <v-progress-linear
-                            :value="50"
-                            class="my-0"
-                            height="3"></v-progress-linear>
+      <v-app id="inspire">
+        <div class="text-center">
+          <v-bottom-sheet inset>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="red" dark v-bind="attrs" v-on="on">
+                Open Player
+              </v-btn>
+            </template>
+            <v-card tile>
+              <v-progress-linear
+                :value="50"
+                class="my-0"
+                height="3"
+              ></v-progress-linear>
 
-                        <v-list>
-                            <v-list-item>
-                                <v-list-item-content>
-                                    <v-list-item-title>The Walker</v-list-item-title>
-                                    <v-list-item-subtitle>Fitz & The Trantrums</v-list-item-subtitle>
-                                </v-list-item-content>
+              <v-list>
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title>The Walker</v-list-item-title>
+                    <v-list-item-subtitle
+                      >Fitz & The Trantrums</v-list-item-subtitle
+                    >
+                  </v-list-item-content>
 
-                                <v-spacer></v-spacer>
+                  <v-spacer></v-spacer>
 
-                                <v-list-item-icon>
-                                    <v-btn icon>
-                                        <v-icon>mdi-rewind</v-icon>
-                                    </v-btn>
-                                </v-list-item-icon>
+                  <v-list-item-icon>
+                    <v-btn icon>
+                      <v-icon>mdi-rewind</v-icon>
+                    </v-btn>
+                  </v-list-item-icon>
 
-                                <v-list-item-icon :class="{ 'mx-5': $vuetify.breakpoint.mdAndUp }">
-                                    <v-btn icon>
-                                        <v-icon>mdi-pause</v-icon>
-                                    </v-btn>
-                                </v-list-item-icon>
+                  <v-list-item-icon
+                    :class="{ 'mx-5': $vuetify.breakpoint.mdAndUp }"
+                  >
+                    <v-btn icon>
+                      <v-icon>mdi-pause</v-icon>
+                    </v-btn>
+                  </v-list-item-icon>
 
-                                <v-list-item-icon
-                                    class="ml-0"
-                                    :class="{ 'mr-3': $vuetify.breakpoint.mdAndUp }">
-                                    <v-btn icon>
-                                        <v-icon>mdi-fast-forward</v-icon>
-                                    </v-btn>
-                                </v-list-item-icon>
-                            </v-list-item>
-                        </v-list>
-                    </v-card>
-                </v-bottom-sheet>
-            </div>
-        </v-app>
+                  <v-list-item-icon
+                    class="ml-0"
+                    :class="{ 'mr-3': $vuetify.breakpoint.mdAndUp }"
+                  >
+                    <v-btn icon>
+                      <v-icon>mdi-fast-forward</v-icon>
+                    </v-btn>
+                  </v-list-item-icon>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-bottom-sheet>
+        </div>
+      </v-app>
     </div>
-</div>
+  </div>
 </template>
 
 <script lang="ts">
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-    name: 'MainPage',
-    components: {
+  name: "MainPage",
+  components: {},
 
-    },
+  data() {
+    return {
+      dialog: false,
+      alignments: ["start", "center", "end"],
+      mainUser: [],
+    };
+  },
+  methods: {},
 
-    data(){
-        return {
-            dialog: false,
-            alignments: [
-                'start',
-                'center',
-                'end',
-            ],
-            mainUser: [
-                
-            ],
-
-        }
-    },
-    methods: {
-        
-
-    },
-
-    async mounted(){
-      let users = await axios.get('http://localhost:3000/user').then((res) => {              
-              console.log(res.data)
-              let user = {
-                  name: res.data.name,
-                  id: res.data._id,
-                  images: res.data.images,
-                  country: res.data.country,
-                  url: res.data.url,
-                  playlists: res.data.playlists[0].items
-              }
-              this.mainUser.push(user) 
-              
-              
+  async mounted() {
+    let users = await axios.get("http://localhost:3000/user").then((res) => {
+    //   console.log(res.data)
+      let user = {
+        name: res.data.name,
+        id: res.data._id,
+        images: res.data.images,
+        country: res.data.country,
+        url: res.data.url,
+        playlists: res.data.playlists[0].items,
       }
-      )
-    }
-    
-    
+      this.mainUser.push(user)
+    })
+  },
 }
 </script>
